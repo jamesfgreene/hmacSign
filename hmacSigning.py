faa6e40a9b4a2cf80d88f97b8021bb139e7b28fc
James Greene <jamesfgreene@gmail.com>
2015-04-08 21:43:16 -0700

    Response returns url in encoded format, along with the checksum (generally should leave encoding of the "url" GET parameter to the application that is generating the request

--- hmacSigning.py
+++ hmacSigning.py
@@ -1,48 +1,52 @@
 from flask import Flask
 from flask import request
 from flask import Response
+from urlparse import urlparse
 import time
 import hmac,hashlib
+import urllib
 global secretKey
 
-secretKey       = 'dirtyLittleSecret'
-app                     = Flask(__name__)
+secretKey	= 'dirtyLittleSecret'
+app			= Flask(__name__)
 
 @app.route("/checksum", methods=['GET'])
-def checksum():
-        requestParamURL = request.args.get('url')
+def checksum():	
+	requestParamURL = request.args.get('url')
 
-        # Set the checksum to expire 60 seconds from now.
-        expireTime              = str(int(time.time()) + 60)
+	urlParsed = urlparse(requestParamURL)
 
-        # Make the expireTime part of the hmac signed data so we can enforce the expiration of the signature
-        hmacData                = requestParamURL+"-"+expireTime
-        checksum                = hmac.new(secretKey, hmacData, hashlib.sha256).hexdigest()+"-"+expireTime
+	# Set the checksum to expire 60 seconds from now.
+	expireTime = str(int(time.time()) + 60)
 
-        responseData = requestParamURL+"&checksum="+checksum
+	# Make the expireTime part of the hmac signed data so we can enforce the expiration of the signature
+	hmacData = requestParamURL+"-"+expireTime
+	checksum = hmac.new(secretKey, hmacData, hashlib.sha256).hexdigest()+"-"+expireTime
 
-        return Response(responseData, status=200, mimetype='text/plain')
+	responseData = urllib.quote_plus(requestParamURL)+"&checksum="+checksum
+	
+	return Response(responseData, status=200, mimetype='text/plain')
 
 @app.route("/checkchecksum", methods=['GET'])
 def checkchecksum():
-        requestParamURL                 = request.args.get('url')
-        requestChecksum                 = request.args.get('checksum')
-        requestChecksumPieces   = requestChecksum.split("-", 1)
-        requestChecksumTime             = requestChecksumPieces[1]
-        requestChecksum                 = requestChecksumPieces[0]
-        currentTime                             = int(time.time())
+	requestParamURL			= request.args.get('url')
+	requestChecksum			= request.args.get('checksum')
+	requestChecksumPieces	= requestChecksum.split("-", 1)
+	requestChecksumTime		= requestChecksumPieces[1]
+	requestChecksum			= requestChecksumPieces[0]
+	currentTime				= int(time.time())
 
-        hmacData        = requestParamURL+"-"+requestChecksumTime
-        checksum        = hmac.new(secretKey, hmacData, hashlib.sha256).hexdigest()
+	hmacData = requestParamURL+"-"+requestChecksumTime
+	checksum = hmac.new(secretKey, hmacData, hashlib.sha256).hexdigest()
 
-        if currentTime <= int(requestChecksumTime) and requestChecksum == checksum:
-                responseData    = "verified"
-                status                  = 200
-        else:
-                responseData    = "not verified"
-                status                  = 400
+	if currentTime <= int(requestChecksumTime) and requestChecksum == checksum:
+		responseData	= "verified"
+		status			= 200
+	else:
+		responseData    = "not verified"
+		status			= 400
 
-        return Response(responseData, status, mimetype='text/plain')
+	return Response(responseData, status, mimetype='text/plain')
 
 if __name__ == "__main__":
     app.run()
