e495288c2539b0bea257efe464617e5e8e703ce9
James Greene <jamesfgreene@gmail.com>
2015-04-08 17:09:42 -0700

    Add the requirements.txt file and python service that performs hmac signing of requests(currently just GET requests)

---
+++ hmacSigning.py
@@ -0,0 +1,49 @@
+from flask import Flask
+from flask import request
+from flask import Response
+import time
+import hmac,hashlib
+global secretKey
+
+secretKey       = 'dirtyLittleSecret'
+app                     = Flask(__name__)
+
+@app.route("/checksum", methods=['GET'])
+def checksum():
+        requestParamURL = request.args.get('url')
+
+        # Set the checksum to expire 60 seconds from now.
+        expireTime              = str(int(time.time()) + 60)
+
+        # Make the expireTime part of the hmac signed data so we can enforce the expiration of the signature
+        hmacData                = requestParamURL+"-"+expireTime
+        checksum                = hmac.new(secretKey, hmacData, hashlib.sha256).hexdigest()+"-"+expireTime
+
+        responseData = requestParamURL+"&checksum="+checksum
+
+        return Response(responseData, status=200, mimetype='text/plain')
+
+@app.route("/checkchecksum", methods=['GET'])
+def checkchecksum():
+        requestParamURL                 = request.args.get('url')
+        requestChecksum                 = request.args.get('checksum')
+        requestChecksumPieces   = requestChecksum.split("-", 1)
+        requestChecksumTime             = requestChecksumPieces[1]
+        requestChecksum                 = requestChecksumPieces[0]
+        currentTime                             = int(time.time())
+
+        hmacData        = requestParamURL+"-"+requestChecksumTime
+        checksum        = hmac.new(secretKey, hmacData, hashlib.sha256).hexdigest()
+
+        if currentTime <= int(requestChecksumTime) and requestChecksum == checksum:
+                responseData    = "verified"
+                status                  = 200
+        else:
+                responseData    = "not verified"
+                status                  = 400
+
+        return Response(responseData, status, mimetype='text/plain')
+
+if __name__ == "__main__":
+    app.run()
+
