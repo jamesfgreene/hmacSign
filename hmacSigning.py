cea5261fb6328e000ec729d643659ce19129aad5
James Greene <jamesfgreene@gmail.com>
2015-04-10 19:52:51 -0700

    Add XSS protection, and errors for the checksum parameter on the checkchecksum API route.  Update requirements.txt

--- hmacSigning.py
+++ hmacSigning.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask import request
 from flask import Response
-from urlparse import urlparse
 import time
 import hmac,hashlib
 import urllib
+import json
+import bleach
+
 global secretKey
 
 secretKey	= 'dirtyLittleSecret'
@@ -12,9 +14,9 @@ app			= Flask(__name__)
 
 @app.route("/checksum", methods=['GET'])
 def checksum():	
-	requestParamURL = request.args.get('url')
-
-	urlParsed = urlparse(requestParamURL)
+	
+	#Get the "url" input parameter and sanitize it, removing ability to use XSS
+	requestParamURL = bleach.clean(request.args.get('url'), strip=True)
 
 	# Set the checksum to expire 60 seconds from now.
 	expireTime = str(int(time.time()) + 60)
@@ -23,30 +25,58 @@ def checksum():
 	hmacData = requestParamURL+"-"+expireTime
 	checksum = hmac.new(secretKey, hmacData, hashlib.sha256).hexdigest()+"-"+expireTime
 
-	responseData = urllib.quote_plus(requestParamURL)+"&checksum="+checksum
+	# This is a convenience so you can quickly paste in the parameters when running the "checkchecksum" operation.
+	verifyChecksumParams = "url="+urllib.quote_plus(requestParamURL)+"&checksum="+checksum
+
+	# JSON encode the data
+	responseData = json.dumps({"url": requestParamURL, "checksum": checksum, "verify_checksum_url":verifyChecksumParams}, sort_keys=True)
 	
-	return Response(responseData, status=200, mimetype='text/plain')
+	return Response(responseData, status=200, mimetype='application/json')
 
 @app.route("/checkchecksum", methods=['GET'])
 def checkchecksum():
-	requestParamURL			= request.args.get('url')
-	requestChecksum			= request.args.get('checksum')
-	requestChecksumPieces	= requestChecksum.split("-", 1)
-	requestChecksumTime		= requestChecksumPieces[1]
+	#Get the "url" input parameter and sanitize it, removing ability to use XSS
+	requestParamURL = bleach.clean(request.args.get('url'), strip=True)
+	
+	#Get the "checksum" input parameter
+	requestChecksum	= request.args.get('checksum')
+	
+	# Verify that the checksum parameter is specified.
+	try:
+		requestChecksumPieces = requestChecksum.split("-", 1)
+	except AttributeError:
+		return Response(json.dumps({"msg":"Missing checksum"}), 400, mimetype='application/json')
+
+	# Verify that the checksum consists of <checksum>-<expirationTime>
+	try:
+		requestChecksumTime	= requestChecksumPieces[1]
+	except IndexError:
+		return Response(json.dumps({"msg":"Invalid checksum"}), 400, mimetype='application/json')
+
+	# Verify that the expirationTime is an integer
+	try:
+		int(requestChecksumTime)
+	except ValueError:
+		return Response(json.dumps({"msg":"Invalid format for checksum time suffix (must be an integer)"}), 400, mimetype='application/json')
+
 	requestChecksum			= requestChecksumPieces[0]
 	currentTime				= int(time.time())
-
+	
+	# Rebuild the checksum based off the input parameters
 	hmacData = requestParamURL+"-"+requestChecksumTime
 	checksum = hmac.new(secretKey, hmacData, hashlib.sha256).hexdigest()
 
+	# Verify that the checksum matches AND that the current time has not exceeded the request expiration time
 	if currentTime <= int(requestChecksumTime) and requestChecksum == checksum:
-		responseData	= "verified"
-		status			= 200
+		verified	= True
+		status		= 200
 	else:
-		responseData    = "not verified"
-		status			= 400
+		verified    = False
+		status		= 400
 
-	return Response(responseData, status, mimetype='text/plain')
+	responseData = json.dumps({"verified":verified});
+	
+	return Response(responseData, status, mimetype='application/json')
 
 if __name__ == "__main__":
     app.run()
